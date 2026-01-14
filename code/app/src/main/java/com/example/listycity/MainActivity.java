d0eeb91e135d529ec47272a1c078ae96df92552c
Youhao Zhao <128596662+youhaozhao@users.noreply.github.com>
2026-01-13 22:10:49 -0700

    feat: add ListyCity for lab 2

---
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ListView cityList;
+    Button addCityButton;
+    Button deleteCityButton;
+    EditText inputCity;
+    Button confirmButton;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    LinearLayout newCityLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+        addCityButton = findViewById(R.id.add_city);
+        deleteCityButton = findViewById(R.id.delete_city);
+        inputCity = findViewById(R.id.input_city);
+        confirmButton = findViewById(R.id.confirm_button);
+        newCityLayout = findViewById(R.id.new_city_layout);
+
+        String[] cities = {"Edmonton", "Montréal"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newCityLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newCity = inputCity.getText().toString().trim();
+                // Add new city if and only if the String is not empty string
+                if (!newCity.isEmpty()) {
+                    inputCity.setText("");
+                    newCityLayout.setVisibility(View.INVISIBLE);
+                    cityAdapter.add(newCity);
+                }
+            }
+        });
+
+        // First, click delete button
+        // Second, choose the city you want to delete from listView
+        // Last, remove city from cityAdpater, then clear the listener
+        deleteCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String deleteCity = cityAdapter.getItem(position);
+                        cityAdapter.remove(deleteCity);
+                        cityList.setOnItemClickListener(null);
+                    }
+                }
+                );
+            }
+        });
+    }
+}
